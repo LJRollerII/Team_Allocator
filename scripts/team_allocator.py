139662f5b68dc6976cc3afcdf5b6d726c8463cb1
LJRollerII <ljcoolkid@optonline.net>
2022-11-12 01:11:14 -0500

    Added new team to allocate.

--- scripts/team_allocator.py
+++ scripts/team_allocator.py
@@ -8,20 +8,29 @@ players = ["John",  "Martin", "Whitney",
            "Zoey", "Darrell", "Erica",
            "William", "Jean", "LLoyd",
            "Maria", "Anthony","Grace",
-           "Natalie", "Xavier"]
+           "Natalie", "Xavier", "Amanda",
+           "Harold", "Penelope", "Lamar",
+           "Carolyn", "Jasper", "Nicole",
+           "Orlando", "Trina", "Gary"]
 
 while True:
     random.shuffle(players)
-    team1 = players[:len(players)//2]
+    team1 = players[:len(players)//3]
     print("Team 1 captain: " + random.choice(team1))
     print("Team 1: ")
     for player in team1:
         print(player)
 
-    team2 = players[len(players)//2:]
+    team2 = players[len(players)//3:(len(players)//3)*2]
     print("\nTeam 2 captain: " + random.choice(team2))
     print("Team 2: ")
     for player in team2:
+        print(player)
+
+    team3 = players[(len(players)//3)*2:]
+    print("\nTeam 3 captain: " + random.choice(team3))
+    print("Team 3: ")
+    for player in team3:
         print(player)
 
     response = input("Pick teams again? Type y or n: ")
